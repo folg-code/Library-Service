b8940c0b752b752efd5f31a19bd45b4d14a928bd
filip <filip1folga@gmail.com>
2026-01-26 17:56:50 +0100

    feat: add borrowing create serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from django.utils.timezone import now
+
+from books.models import Book
 from .models import Borrowing
 from books.serializers import BookReadSerializer
 
@@ -17,3 +20,29 @@ class BorrowingReadSerializer(serializers.ModelSerializer):
             "actual_return_date",
             "is_active",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "book",
+            "expected_return_date",
+        )
+
+    def validate_expected_return_date(self, value):
+        if value <= now().date():
+            raise serializers.ValidationError(
+                "Expected return date must be in the future."
+            )
+        return value
+
+    def validate(self, attrs):
+        book: Book = attrs["book"]
+
+        if book.inventory <= 0:
+            raise serializers.ValidationError(
+                "Book is not available."
+            )
+
+        return attrs
