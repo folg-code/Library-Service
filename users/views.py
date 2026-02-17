21aad7157f84147e5cde6c59a3e78c252a606505
filip <filip1folga@gmail.com>
2026-02-17 14:19:02 +0100

    feat: implement user viewset with me endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserRegisterSerializer, UserReadSerializer
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return UserRegisterSerializer
+        return UserReadSerializer
+
+    @action(methods=["get", "put", "patch"], detail=False)
+    def me(self, request):
+        if request.method == "GET":
+            serializer = UserReadSerializer(request.user)
+            return Response(serializer.data)
+
+        serializer = UserReadSerializer(
+            request.user,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
