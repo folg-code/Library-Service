6841564d5bf35369e0ec036760d437d1b6805d1f
filip <filip1folga@gmail.com>
2026-02-17 14:19:18 +0100

    feat: add jwt token endpoints and user routing

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,16 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-urlpatterns = []
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register("", UserViewSet, basename="users")
+
+urlpatterns = router.urls + [
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+]
